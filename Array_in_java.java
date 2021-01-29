1c5381806293c3dbf0d72e4d5029a23cc8d10218
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-01-29 13:30:58 +0530

    Update Array_in_java.java

--- Array_in_java.java
+++ Array_in_java.java
@@ -8,11 +8,23 @@ public class Array{
 	System.out.println(sentence);
 }
 
+
+		
 		package strings;
 public class StringIntroduction {
 public static void main(String[] args)
 {
 String name="Shlok";
 System.out.println(name.charAt[0]);
+}
+}
+
+		
+		package strings;
+public class StringIntroduction {
+public static void main(String[] args)
+{
+String name="Shlok";
+System.out.println(name.length());
 }
 }
